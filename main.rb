81e7d39da10c16b9e933a4b6bfef64b5f6327103
Saoki11111 <t.aoki11111@gmail.com>
2020-07-27 22:44:50 +0900

    db からデータを抽出

--- main.rb
+++ main.rb
@@ -18,11 +18,15 @@ ActiveRecord::Base.establish_connection(
 class User < ActiveRecord::Base
 end
 
-# insert
+User.delete_all # ファイルが実行されるたびに挿入されるのでリセット
+User.create(name: "tanaka", age: 19)
+User.create(name: "takahashi", age: 25)
+User.create(name: "hayashi", age: 31)
+User.create(name: "mizutani", age: 28)
+User.create(name: "otsuka", age: 35)
 
-user = User.new do |u|
-  u.name = "mochizuki"
-  u.age = 18
-end
-
-user.save
+# pp User.all # require pp
+# pp User.select("id, name, age").all # 全件の id name age
+# pp User.select("id, name, age").first # 先頭
+# pp User.select("id, name, age").last # 最後
+pp User.select("id, name, age").first(3) # 最初の三件
