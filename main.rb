745e0270ab3d6f58e37cd20e1d6b6f27eaca1794
Saoki11111 <t.aoki11111@gmail.com>
2020-07-28 09:21:50 +0900

    find_by で フィールドを指定して抽出

--- main.rb
+++ main.rb
@@ -19,14 +19,19 @@ class User < ActiveRecord::Base
 end
 
 User.delete_all # ファイルが実行されるたびに挿入されるのでリセット
+
 User.create(name: "tanaka", age: 19)
 User.create(name: "takahashi", age: 25)
 User.create(name: "hayashi", age: 31)
 User.create(name: "mizutani", age: 28)
 User.create(name: "otsuka", age: 35)
 
-# pp User.all # require pp
-# pp User.select("id, name, age").all # 全件の id name age
-# pp User.select("id, name, age").first # 先頭
-# pp User.select("id, name, age").last # 最後
-pp User.select("id, name, age").first(3) # 最初の三件
+# pp User.find(3) # id が 3 のレコード
+# pp User.select("id, name, age").find(3) #id が 3 のフィールド とレコード
+# pp User.select("id, name, age").find_by(name: "tanaka") # name = tanaka のレコード
+# pp User.select("id, name, age").find_by name: "tanaka" # ↑ の省略形
+# pp User.select("id, name, age").find_by_name "tanaka" # ↑ と同様
+# pp User.select("id, name, age").find_by_name("tanaka") # ↑ と同様
+
+pp User.select("id, name, age").find_by_name("kiriya") # 存在しないフィールドは nill になる
+pp User.select("id, name, age").find_by_name!("kiriya") # 存在しないフィールドでは エラーを出す
